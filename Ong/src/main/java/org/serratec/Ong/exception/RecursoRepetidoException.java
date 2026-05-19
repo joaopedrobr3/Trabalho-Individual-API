67433899991a817ddd36211aac07754d0aa57d2c
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-05-19 17:26:34 -0300

    feat: Criando a exception de Recurso Repetido

---
+++ Ong/src/main/java/org/serratec/Ong/exception/RecursoRepetidoException.java
@@ -0,0 +1,11 @@
+package org.serratec.Ong.exception;
+
+public class RecursoRepetidoException extends RuntimeException {
+
+    public RecursoRepetidoException(String message) {
+        super(message);
+    }
+
+    
+   
+}
